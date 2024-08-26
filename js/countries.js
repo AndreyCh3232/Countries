d76530c4575de7bbfb1ee7fa55813a413753016c
Andrey Chetvergov <andrey32102@gmail.com>
2024-08-26 20:23:06 +0300

    Add func onGetCountryInfo

--- js/countries.js
+++ js/countries.js
@@ -1,7 +1,21 @@
 'use strict'
 
 function onGetCountryInfo() {
-    console.log('Hi')
+    const countryName = $('#country-input').val().trim()
+    if (countryName) {
+        $.ajax({
+            url: `https://restcountries.com/v3.1/name/${countryName}`,
+            method: 'GET',
+            success: function (data) {
+                renderInfo(data);
+            },
+            error: function () {
+                $('#country-info').text('Country not found.')
+            }
+        })
+    } else {
+        alert('Please enter a country name.')
+    }
 }
 
 function renderInfo(data) {
